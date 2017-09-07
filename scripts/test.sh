e22981e76b7bf3d1739727aab83fdc6a76fe5469
Katarzyna Kujawa <katarzyna.kujawa@intel.com>
2017-09-07 15:37:35 +0200

    Update from snap-pluginsync 0.1.14

--- scripts/test.sh
+++ scripts/test.sh
@@ -88,7 +88,13 @@ elif [[ $TEST_TYPE == "medium" ]]; then
     test_unit
   fi
 elif [[ $TEST_TYPE == "large" ]]; then
-	echo "Error - there are no large tests"
+  if [[ "${TEST_K8S}" != "0" && -f "$__dir/large_k8s.sh" ]]; then
+    . "${__dir}/large_k8s.sh"
+  elif [[ -f "${__dir}/large_compose.sh" ]]; then
+    . "${__dir}/large_compose.sh"
+  else
+    . "${__dir}/large.sh"
+  fi
 elif [[ $TEST_TYPE == "build" ]]; then
   "${__dir}/build.sh"
 fi
